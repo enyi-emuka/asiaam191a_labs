e0dcd5744f9c943f37f9dfdd600aaf76905bbfee
Enyi Emuka <crystalemuka@g.ucla.edu>
2023-04-25 14:06:17 -0700

    lab 2

--- week2/js/init.js
+++ week2/js/init.js
@@ -4,9 +4,8 @@
 const map = L.map('the_map').setView([34.0709, -118.444], 15); 
 
 // Leaflet tile layer, i.e. the base map
-let Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map); 
 
 //JavaScript let variable declaration to create a marker
